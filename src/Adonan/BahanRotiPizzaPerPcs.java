4fa5f19ff92cfb8ad3dbbb954a380734e42097d8
Bobby5798 <117921139+Bobby5798@users.noreply.github.com>
2023-06-22 20:51:06 +0700

    BahanBahanRotipart2

--- src/Adonan/BahanRotiPizzaPerPcs.java
+++ src/Adonan/BahanRotiPizzaPerPcs.java
@@ -5,49 +5,49 @@ import BahanRoti.RotiTawar;
 public class BahanRotiPizzaPerPcs extends RotiTawar {
     @Override
     public double tepungTerigu() {
-        int total = 1950/50;
+        int total = 1950/190;
         return 1000/total;
     }
 
     @Override
     public double gulaPasir() {
-        int total = 1950/50;
+        int total = 1950/190;
         return 100/total;
     }
 
     @Override
     public double butter() {
-        int total = 1950/50;
+        int total = 1950/190;
         return 100/total;
     }
 
     @Override
     public double ragi() {
-        int total = 1950/50;
+        int total = 1950/190;
         return 20/total;
     }
 
     @Override
     public double susuBubuk() {
-        int total = 1950/50;
+        int total = 1950/190;
         return 200/total;
     }
 
     @Override
     public double susuCair() {
-        int total = 1950/50;
+        int total = 1950/190;
         return 180/total;
     }
 
     @Override
     public double telur() {
-        int total = 1950/50;
+        int total = 1950/190;
         return 50/total;
     }
 
     @Override
     public double esBatu() {
-        int total = 1950/50;
+        int total = 1950/190;
         return 300/total;
     }
 }
--- src/Adonan/BahanRotiTawarPerPcs.java
+++ src/Adonan/BahanRotiTawarPerPcs.java
@@ -5,49 +5,49 @@ import BahanRoti.RotiTawar;
 public class BahanRotiTawarPerPcs extends RotiTawar {
     @Override
     public double tepungTerigu() {
-        int total = 2065/50;
+        int total = 2065/400;
         return 1000/total;
     }
 
     @Override
     public double gulaPasir() {
-        int total = 2065/50;
+        int total = 2065/400;
         return 125/total;
     }
 
     @Override
     public double butter() {
-        int total = 2065/50;
+        int total = 2065/400;
         return 100/total;
     }
 
     @Override
     public double ragi() {
-        int total = 2065/50;
+        int total = 2065/400;
         return 20/total;
     }
 
     @Override
     public double susuBubuk() {
-        int total = 2065/50;
+        int total = 2065/400;
         return 250/total;
     }
 
     @Override
     public double susuCair() {
-        int total = 2065/50;
+        int total = 2065/400;
         return 200/total;
     }
 
     @Override
     public double telur() {
-        int total = 2065/50;
+        int total = 2065/400;
         return 70/total;
     }
 
     @Override
     public double esBatu() {
-        int total = 2065/50;
+        int total = 2065/400;
         return 300/total;
     }
 }
