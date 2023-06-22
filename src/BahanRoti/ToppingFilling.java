bc70014140c55297bd055c30e870a91e2c22ff3f
Septian Dwi Cahyo Nugroho <septiandwi@student.uns.ac.id>
2023-06-22 19:45:54 +0700

    ClassToppingFilling

---
+++ src/BahanRoti/ToppingFilling.java
@@ -0,0 +1,11 @@
+package BahanRoti;
+
+public abstract class ToppingFilling {
+    double keju;
+    double coklat;
+    double krimVanilla;
+    double Redbean;
+    double sosis;
+    double smokeBeef;
+    double bawangBombay;
+}
