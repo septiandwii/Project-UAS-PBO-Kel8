c291aa339ace56030e82662870f7948117e3536b
Septian Dwi Cahyo Nugroho <septiandwi@student.uns.ac.id>
2023-06-23 22:15:05 +0700

    update

--- src/Adonan/Sisa.java
+++ src/Adonan/Sisa.java
@@ -10,7 +10,8 @@ public class Sisa {
     double susuCair = 0;
     double telur = 0;
     double esBatu = 0;
-//    public void cekBahan(double tepungTerigu){
+
+    //    public void cekBahan(double tepungTerigu){
 //        if(this.tepungTerigu < tepungTerigu){
 //            System.out.println("Tepung terigu sebelum kulakan"+this.tepungTerigu);
 //            this.tepungTerigu += dbahanTambahan.tepungTerigu();
@@ -128,4 +129,18 @@ public class Sisa {
 //            System.out.println("Es Batu setelah dikurangi butuh" + this.esBatu);
 //        }
 //    }
+    public void cekBahan(double tepungTerigu) {
+        if (this.tepungTerigu < tepungTerigu) {
+            System.out.println("Tepung terigu sebelum kulakan" + this.tepungTerigu);
+            this.tepungTerigu += bahanTambahan.tepungTerigu();
+            System.out.println("Tepung terigu setelah kulakan" + this.tepungTerigu);
+            tepungTerigu -= this.tepungTerigu;
+            System.out.println("tepung terigu dibutuhkan: " + tepungTerigu);
+            cekBahan(tepungTerigu);
+
+        } else {
+            this.tepungTerigu -= tepungTerigu;
+            System.out.println("Tepung terigu setelah dikurangi butuh: " + this.tepungTerigu);
+        }
+    }
 }
