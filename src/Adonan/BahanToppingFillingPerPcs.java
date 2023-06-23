3d3bbe8a73ac214fecc998b6a627a987c1468bd5
Septian Dwi Cahyo Nugroho <septiandwi@student.uns.ac.id>
2023-06-23 21:00:30 +0700

    update

--- src/Adonan/BahanToppingFillingPerPcs.java
+++ src/Adonan/BahanToppingFillingPerPcs.java
@@ -5,43 +5,36 @@ import BahanRoti.ToppingFilling;
 public class BahanToppingFillingPerPcs extends ToppingFilling {
     @Override
     public double keju() {
-
         return 250;
     }
 
     @Override
     public double coklat() {
-
         return 500;
     }
 
     @Override
     public double krimVanilla() {
-
         return 500;
     }
 
     @Override
     public double Redbean() {
-
         return 500;
     }
 
     @Override
     public double sosis() {
-
         return 1000;
     }
 
     @Override
     public double smokeBeef() {
-
         return 1000;
     }
 
     @Override
     public double bawangBombay() {
-
         return 500;
     }
 }
