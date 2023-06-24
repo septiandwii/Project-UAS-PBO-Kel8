6154be0a299499180f080953d2e636e49d10fb3a
Septian Dwi Cahyo Nugroho <septiandwi@student.uns.ac.id>
2023-06-24 21:41:02 +0700

    update

--- src/Adonan/Main.java
+++ src/Adonan/Main.java
@@ -10,5 +10,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        BahanRotiManisPerPcs manis = new BahanRotiManisPerPcs();
+        Sisa sisa = new Sisa();
+        double butuh = 100.0;
+        sisa.cekBahan(manis.tepungTerigu()*butuh, manis.gulaPasir()*butuh, manis.butter() * butuh, manis.ragi()*butuh, manis.susuBubuk()*butuh, manis.susuCair()*butuh, manis.telur()*butuh, manis.esBatu()*butuh);
+        System.out.println(sisa.modal);
     }
 }
